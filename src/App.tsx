9e036c5d0f7907964b8cecc9673e8c9cf2f8bb76
sese <dev.shhong@gmail.com>
2025-03-18 23:01:04 +0900

    add route setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NotFound from "./page/NotFound";
+import List from "./page/List";
+import Test from "./page/Test";
+import TestResult from "./page/TestResult";
 
+function App() {
   return (
-    <div><p>MBTI TEST</p></div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<List />} />
+        <Route path="/:testParam" element={<Test />} />
+        <Route
+          path="/:testParam/result/:resultParam"
+          element={<TestResult />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
