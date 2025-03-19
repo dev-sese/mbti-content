3268013eee3d8062f59e5dd0c428a6b58e3d4d0c
sese <dev.shhong@gmail.com>
2025-03-20 00:28:39 +0900

    별자리 테스트로 데이터 변경

--- src/components/testResult/TestResultRenderer.tsx
+++ src/components/testResult/TestResultRenderer.tsx
@@ -7,8 +7,16 @@ const TestResultRenderer = ({
 }) => {
   return (
     <div>
-      <h3 className="text-3xl font-bold">결과는...</h3>
-      <span>{typeResult?.type}</span>
+      <h3 className="text-3xl font-bold my-4">결과는...</h3>
+      <img
+        src={typeResult?.img_src}
+        alt={typeResult?.zodiac}
+        className="w-2/5 m-auto rounded-lg"
+      />
+      <div className="w-3/5 m-auto text-center my-4">
+        <p className="mb-3 text-xl font-semibold">{typeResult?.zodiac}</p>
+        <p>{typeResult?.description}</p>
+      </div>
     </div>
   );
 };
